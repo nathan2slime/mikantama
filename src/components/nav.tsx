9967fd63f756761bbc31bad7e087aed7f93142d4
Jhonathan M <nathan3boss@gmail.com>
2025-02-27 20:25:55 -0300

    feat: remove shadows

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -9,9 +9,9 @@ import { Button } from '~/components/ui/button'
 
 export const Nav = () => {
   return (
-    <div className="w-full right-1 z-10 h-16 border-b px-4 flex justify-between items-center gap-2 border-border bg-muted-foreground/90 backdrop-blur-md sticky top-0">
+    <div className="w-full right-1 z-10 rounded-br-lg rounded-bl-lg h-16 border-b px-4 flex justify-between items-center gap-2 border-border bg-muted-foreground/90 backdrop-blur-md sticky top-0">
       <Link href="/home">
-        <Planet size={120} className="translate-y-3" color="#C4A7E7" />
+        <Planet size={120} className="translate-y-3 -translate-x-6" color="#C4A7E7" />
       </Link>
 
       <div className="flex items-center gap-2 justify-center">
